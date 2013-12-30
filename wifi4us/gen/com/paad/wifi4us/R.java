9b35769fc6838db466270df178486c419932b291
mylich119 <mylich119@126.com>
2013-12-31 00:21:32 +0800

    test for cm line

--- wifi4us/gen/com/paad/wifi4us/R.java
+++ wifi4us/gen/com/paad/wifi4us/R.java
@@ -21,14 +21,26 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int homeimage=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int receive_draw_1=0x7f020002;
-        public static final int receive_draw_2=0x7f020003;
-        public static final int send_draw_1=0x7f020004;
+        public static final int button_shape=0x7f020000;
+        public static final int homeimage=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int logo=0x7f020003;
+        public static final int other=0x7f020004;
+        public static final int receive_draw_1=0x7f020005;
+        public static final int receive_draw_2=0x7f020006;
+        public static final int search_off=0x7f020007;
+        public static final int search_on=0x7f020008;
+        public static final int send_draw_1=0x7f020009;
+        public static final int settings_off=0x7f02000a;
+        public static final int settings_on=0x7f02000b;
+        public static final int share_off=0x7f02000c;
+        public static final int share_on=0x7f02000d;
+        public static final int tab_search_selector=0x7f02000e;
+        public static final int tab_settings_selector=0x7f02000f;
+        public static final int tab_share_selector=0x7f020010;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001a;
+        public static final int action_settings=0x7f08001c;
         public static final int imageView=0x7f080000;
         public static final int main_tab=0x7f080001;
         public static final int other=0x7f080004;
@@ -53,6 +65,8 @@ public final class R {
         public static final int send_text_client_info=0x7f080019;
         public static final int send_text_time_left=0x7f080017;
         public static final int send_text_traffic_used=0x7f080018;
+        public static final int tabimage=0x7f08001a;
+        public static final int tabtitle=0x7f08001b;
         public static final int userid_text=0x7f08000a;
         public static final int videoview=0x7f080005;
     }
@@ -76,6 +90,7 @@ public final class R {
         public static final int fragment_send_stop_share_button=0x7f030010;
         public static final int fragment_send_wifi_connected_state=0x7f030011;
         public static final int fragment_wifi_progressbar=0x7f030012;
+        public static final int tab_card=0x7f030013;
     }
     public static final class menu {
         public static final int main=0x7f070000;
