d7c614a0d0f958a0d2d724896497706ec551a1a2
mylich119 <mylich119@126.com>
2014-01-02 14:29:48 +0800

    setup the socket connection for two devices

---
+++ wifi4us/src/com/paad/wifi4us/SendStopShareText.java
@@ -0,0 +1,14 @@
+package com.paad.wifi4us;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SendStopShareText extends Fragment{
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
+		return inflater.inflate(R.layout.fragment_send_stop_share_text, container, false);
+	}
+
+}
