7dc807604ff145ebb2e4bf6227d16459ba892a41
yangshi981 <yangshi980@gmail.com>
2014-01-09 03:15:53 -0800

    MOD:add semi-finished other fragment

---
+++ wifi4us/src/com/paad/wifi4us/FeedbackActivity.java
@@ -0,0 +1,83 @@
+/**
+ * @(#)FeedbackActivity.java, 2014-1-6. 
+ * 
+ * Copyright 2014 Yodao, Inc. All rights reserved.
+ * YODAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.paad.wifi4us;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+
+import com.paad.wifi4us.R;
+
+/**
+ *
+ * @author yangshi
+ *
+ */
+public class FeedbackActivity  extends Activity implements OnClickListener{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+        findViewById(R.id.btn_send_feedback).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String msg = ((EditText)findViewById(R.id.view_feedback_msg)).toString();
+        String contact = ((EditText)findViewById(R.id.view_feedback_contact)).toString();
+        postFeedback(msg, contact);
+    }
+    
+    public boolean postFeedback(String feedback, String contact) {
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        HttpPost post = new HttpPost(Constants.SERVER_PREFIX);
+        List<BasicNameValuePair> postData = new ArrayList<BasicNameValuePair>();
+        postData.add(new BasicNameValuePair("msg", feedback));
+        postData.add(new BasicNameValuePair("contact", contact));
+        UrlEncodedFormEntity entity = null;
+        try {
+            entity = new UrlEncodedFormEntity(postData, HTTP.UTF_8);
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+            return false;
+        }
+        post.setEntity(entity);
+        HttpResponse response = null;
+        try {
+            response = httpClient.execute(post);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        if (response.getStatusLine().getStatusCode() == 200) {
+            return true;
+        }
+        return false;
+    }
+
+}
--- wifi4us/src/com/paad/wifi4us/OtherFragment.java
+++ wifi4us/src/com/paad/wifi4us/OtherFragment.java
@@ -1,83 +1,178 @@
 package com.paad.wifi4us;
 
+import java.io.InputStream;
+
+import org.apache.http.util.EncodingUtils;
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.Process;
 import android.provider.Settings.Secure;
 import android.support.v4.app.Fragment;
 import android.support.v4.util.SimpleArrayMap;
 import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import com.paad.wifi4us.utility.HttpXmlParser;
 
-public class OtherFragment extends Fragment{
-	private TextView other_id_userid_text;
-	private static final int MSG_SUCCESS = 0; 
-    private static final int MSG_FAILURE = 1;  
-    private static final String REGISTER_BASE_HTTPURL= "http://wifi4us.duapp.com/register.php";
-	
-    private Handler mHandler = new Handler(){  
-		public void handleMessage (Message msg){
-			switch(msg.what){  
-				case MSG_SUCCESS:  
-					other_id_userid_text.setText("ÓÃ»§Ãû£º " + (String) msg.obj);
-					Editor sharedata = getActivity().getSharedPreferences(getActivity().getApplicationContext().getPackageName(), Context.MODE_PRIVATE).edit(); 
-	        		sharedata.putString("USER_ID", (String) msg.obj);
-	        		sharedata.commit();
-	                break;  
-	            case MSG_FAILURE:  
-	            	other_id_userid_text.setText("»ñÈ¡idÊ§°Ü£¬ÎÞ·¨Ê¹ÓÃ·þÎñ£¬Çë±£³ÖÍøÂ·³©Í¨£¬ÍË³öÈí¼þÖØÐÂ½øÈë");
-	                break;  
-			}  
-		}  
-    };    
-    
-    Runnable getUserIdRunner = new Runnable() {   
-    	public void run() { 
-    		
-    			TelephonyManager tm = (TelephonyManager)getActivity().getSystemService(Context.TELEPHONY_SERVICE);
-    			String registerUrl;
-        		if(tm.getDeviceId() == null){
-        			registerUrl = REGISTER_BASE_HTTPURL + "?" + "imei=" + Secure.getString(getActivity().getApplicationContext().getContentResolver(), Secure.ANDROID_ID);
-        		}else{
-        			registerUrl = REGISTER_BASE_HTTPURL + "?" + "imei=" + tm.getDeviceId();
-        		}
-        		HttpXmlParser xpp = new HttpXmlParser();
-        		SimpleArrayMap<String, String> result = new SimpleArrayMap<String, String>();
+public class OtherFragment extends Fragment implements OnClickListener {
+    private TextView other_id_userid_text;
 
-        		if(xpp.getResultFromURL(registerUrl, result)){
-            		String userid = result.get("userid");
-            		mHandler.obtainMessage(MSG_SUCCESS, userid).sendToTarget();             
-        		}else{
-            		mHandler.obtainMessage(MSG_FAILURE).sendToTarget();             
-        		}
+    private static final int MSG_SUCCESS = 0;
 
-			
-        }  
-    }; 
-	 
-		
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
-		super.onCreateView(inflater, container, savedInstanceState);
-		View view_res = inflater.inflate(R.layout.fragment_other, container, false);
-		
-		other_id_userid_text = (TextView)view_res.findViewById(R.id.userid_text);
-		SharedPreferences sharedata = getActivity().getSharedPreferences(getActivity().getApplicationContext().getPackageName(), Context.MODE_PRIVATE); 
-		String userid = sharedata.getString("USER_ID", "NULL");
-		other_id_userid_text.setText("ÓÃ»§Ãû£º " + userid);
-		if(userid.equals("NULL")){
-			Thread mThread = new Thread(getUserIdRunner);  
+    private static final int MSG_FAILURE = 1;
+
+    private static final String REGISTER_BASE_HTTPURL = "http://wifi4us.duapp.com/register.php";
+
+    private Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SUCCESS:
+                    other_id_userid_text.setText("ï¿½Ã»ï¿½ï¿½ï¿½ " + (String) msg.obj);
+                    Editor sharedata = getActivity().getSharedPreferences(
+                            getActivity().getApplicationContext()
+                                    .getPackageName(), Context.MODE_PRIVATE)
+                            .edit();
+                    sharedata.putString("USER_ID", (String) msg.obj);
+                    sharedata.commit();
+                    break;
+                case MSG_FAILURE:
+                    other_id_userid_text
+                            .setText("ï¿½ï¿½È¡idÊ§ï¿½Ü£ï¿½ï¿½Þ·ï¿½Ê¹ï¿½Ã·ï¿½ï¿½ï¿½ï¿½ë±£ï¿½ï¿½ï¿½ï¿½Â·ï¿½ï¿½Í¨ï¿½ï¿½ï¿½Ë³ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Â½ï¿½ï¿½ï¿½");
+                    break;
+            }
+        }
+    };
+
+    Runnable getUserIdRunner = new Runnable() {
+        public void run() {
+
+            TelephonyManager tm = (TelephonyManager) getActivity()
+                    .getSystemService(Context.TELEPHONY_SERVICE);
+            String registerUrl;
+            if (tm.getDeviceId() == null) {
+                registerUrl = REGISTER_BASE_HTTPURL
+                        + "?"
+                        + "imei="
+                        + Secure.getString(getActivity()
+                                .getApplicationContext().getContentResolver(),
+                                Secure.ANDROID_ID);
+            } else {
+                registerUrl = REGISTER_BASE_HTTPURL + "?" + "imei="
+                        + tm.getDeviceId();
+            }
+            HttpXmlParser xpp = new HttpXmlParser();
+            SimpleArrayMap<String, String> result = new SimpleArrayMap<String, String>();
+
+            if (xpp.getResultFromURL(registerUrl, result)) {
+                String userid = result.get("userid");
+                mHandler.obtainMessage(MSG_SUCCESS, userid).sendToTarget();
+            } else {
+                mHandler.obtainMessage(MSG_FAILURE).sendToTarget();
+            }
+
+        }
+    };
+
+    String userid;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view_res = inflater.inflate(R.layout.fragment_other, container,
+                false);
+
+        SharedPreferences sharedata = getActivity().getSharedPreferences(
+                getActivity().getApplicationContext().getPackageName(),
+                Context.MODE_PRIVATE);
+        userid = sharedata.getString("USER_ID", "NULL");
+        view_res.findViewById(R.id.btn_quit).setOnClickListener(this);
+        view_res.findViewById(R.id.btn_about).setOnClickListener(this);
+        view_res.findViewById(R.id.btn_check_update).setOnClickListener(this);
+        view_res.findViewById(R.id.btn_feed_back).setOnClickListener(this);
+        view_res.findViewById(R.id.btn_help).setOnClickListener(this);
+        view_res.findViewById(R.id.btn_Set_Discuss).setOnClickListener(this);
+        view_res.findViewById(R.id.btn_Set_Score).setOnClickListener(this);
+        view_res.findViewById(R.id.btn_settings).setOnClickListener(this);
+        view_res.findViewById(R.id.btn_agreement).setOnClickListener(this);
+        if (userid.equals("NULL")) {
+            Thread mThread = new Thread(getUserIdRunner);
             mThread.start();
-		}
-		
-		return view_res;
-	}
+        }
+
+        return view_res;
+    }
+
+    protected void showWebViewDialog(String filename) {
+        try {
+            Builder builder = new AlertDialog.Builder(getActivity());
+            WebView webView = new WebView(getActivity());
+            InputStream inputstream;
+            String s1;
+            inputstream = getResources().getAssets().open(filename);
+            byte abyte0[] = new byte[inputstream.available()];
+            inputstream.read(abyte0);
+            s1 = EncodingUtils.getString(abyte0, "UTF-8");
+            String s = s1;
+            inputstream.close();
+            webView.loadDataWithBaseURL(null, s, "text/html", "utf-8", null);
+            builder.setView(webView);
+            builder.setPositiveButton(
+                    getResources().getText(R.string.webview_back), null);
+            builder.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void quit() {
+        getActivity().finish();
+        Process.killProcess(Process.myPid());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_about:
+                try {
+                    showWebViewDialog("about.html");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case R.id.btn_agreement:
+                try {
+                    showWebViewDialog("agreement.html");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btn_quit:
+                quit();
+                break;
+            case R.id.btn_feed_back:
+                startActivity(new Intent(getActivity(), FeedbackActivity.class));
+                break;
+            case R.id.btn_settings:
+                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                break;
+            default:
+                break;
+        }
+
+    }
 
 }
